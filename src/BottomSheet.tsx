e90ded2ce6d8c9189046d66845dfc8474ea10bfb
Jason <jasaaanlim@gmail.com>
2022-06-06 09:16:45 +0800

    Expose header height

--- src/BottomSheet.tsx
+++ src/BottomSheet.tsx
@@ -443,6 +443,9 @@ export const BottomSheet = React.forwardRef<
       get height() {
         return heightRef.current
       },
+      get headerHeight() {
+        return headerRef
+      }
     }),
     [send]
   )
